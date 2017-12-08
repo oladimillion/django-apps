7570c59ae56383dbc27441ce739f783e24764dcb
Akande Oladimeji <akandeoladimeji9@hotmail.com>
2017-12-08 23:35:28 +0100

    removeS static route

--- bolt/urls.py
+++ bolt/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import url
-from mysite.settings  import settings
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^api/welcome/$', views.welcome, name="welcome"),
     url(r'^api/post/$', views.post, name="post"),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 ]
