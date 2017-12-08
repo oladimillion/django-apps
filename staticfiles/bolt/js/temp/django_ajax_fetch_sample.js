a6be21f094561d8849b156a43fbafb3348c52539
Akande Oladimeji <akandeoladimeji9@hotmail.com>
2017-12-09 00:20:00 +0100

    added staticfiles folder

---
+++ staticfiles/bolt/js/temp/django_ajax_fetch_sample.js
@@ -0,0 +1,42 @@
+
+let url = "/bolt/api/post/";
+
+function status(res){
+  if(res.status >= 200 && res.status < 300){
+    return Promise.resolve(res)
+  }
+  else {
+    return Promise.reject(new Error(res.statusText))
+  }
+}
+
+function json(res){
+  return res.json();
+}
+
+function display(data){
+  console.log(data)
+}
+
+
+let payload = {name: "oladimeji"};
+let data = new FormData()
+data.append("post", JSON.stringify(payload));
+
+function post(){
+  fetch(url, {
+  method: "post",
+    body: data
+  })
+  .then(status)
+  .then(json)
+  .then(display)
+  .catch(display)
+}
+
+function onclick(event){
+  post();
+}
+
+const sendButton = document.getElementById("send");
+sendButton.onclick = onclick;
